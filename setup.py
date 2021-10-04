78bf720099c88dcfb16e8751fdf5019e19c3bf70
aldiak <aldiak95@gmail.com>
2021-10-04 08:35:53 +0800

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is a machine learning end to end project using mlops',
+    author='ALOU',
+    license='MIT',
+)
